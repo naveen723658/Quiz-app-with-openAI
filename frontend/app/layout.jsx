963d3e326d6f4a7a136d4948b4e22099e07938d0
Naveen Kumar <naveen.kumar723658@gmail.com>
2023-05-20 22:48:56 +0530

    pages modified

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -2,7 +2,7 @@ import '../styles/style.css';
 export default function RootLayout({ children }) {
     return (
       <html lang="en">
-        <body className='bg-gradient-to-r from-slate-900 to-slate-700'>{children}</body>
+        <body className='bg-gradient-to-r from-slate-900 to-slate-700 h-screen text-slate-50'>{children}</body>
       </html>
     );
   }
---
+++ frontend/app/signin/page.jsx
@@ -0,0 +1,11 @@
+import Authentication from "../../components/home";
+import Background from "../../components/background";
+
+export default function Page() {
+  return (
+    <>
+      <Background />
+      <Authentication />
+    </>
+  );
+}
